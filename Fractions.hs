616188dbfd2ffcd4de196d46ac40b007e519fc67
Edward Kmett <ekmett@gmail.com>
2015-04-08 13:50:12 -0400

    getting ready to do continued fractions of continued fractions

--- Fractions.hs
+++ Fractions.hs
@@ -88,35 +88,6 @@ convergents (CF xs0) = go 1 0 0 1 xs0 where
   go a b c d (y:ys) = fromRational (e :% f) : go e a f c ys
     where e = a*y+b; f = c*y+d
 
--- | Gosper-style bihomographic transformations
---
--- @
--- z = axy + bx + cy + d
---     -----------------
---     exy + fx + gy + h
--- @
---
--- TODO: detect cycles
-bihom :: Integer -> Integer -> Integer -> Integer
-      -> Integer -> Integer -> Integer -> Integer -> CF -> CF -> CF
-bihom = coerce go where
-  go :: Integer -> Integer -> Integer -> Integer
-     -> Integer -> Integer -> Integer -> Integer -> [Integer] -> [Integer] -> [Integer]
-  go a b _ _ e f _ _ xs [] = coefs $ hom a b e f (CF xs)
-  go a _ c _ e _ g _ [] ys = coefs $ hom a c e g (CF ys)
-  go 0 1 0 0 0 0 0 1 xs _  = xs
-  go 0 0 1 0 0 0 0 1 _  ys = ys
-  go a b c d e f g h xs@(x:xs') ys@(y:ys')
-     | e /= 0, f /= 0, g /= 0, h /= 0 
-     , q <- quot a e, q == quot b f
-     , q == quot c g, q == quot d h
-     = q : go e f g h (a-q*e) (b-q*f) (c-q*g) (d-q*h) xs ys
-     | e /= 0 || f /= 0
-     , (e == 0 && g == 0) || abs (g*e*b - g*a*f) > abs (f*e*c - g*a*f)
-     = go (a*x+b) a (c*x+d) c (e*x+f) e (g*x+h) g xs' ys
-     | otherwise
-     = go (a*y+c) (b*y+d) a b (e*y+g) (f*y+h) e f xs ys'
-
 -- | 
 -- @
 -- z = hom a b c d
@@ -135,32 +106,70 @@ bihom = coerce go where
 -- TODO: detect cycles, once we do, detect cycle length, then our position in it
 -- this will let us detect the length of the cycle we emit.
 hom :: Integer -> Integer -> Integer -> Integer -> CF -> CF
-hom = coerce go where
-  go :: Integer -> Integer -> Integer -> Integer -> [Integer] -> [Integer]
-  go 1 0 0 1 xs = xs
-  go _ _ 0 0 _  = []
-  go a b c d xs
-    | c /= 0, d /= 0
-    , q <- quot a c, q == quot b d
-    = q : go c d (a - c*q) (b - d*q) xs
-    | otherwise = case xs of
-      []   -> go a a c c []
-      y:ys -> go (a*y+b) a (c*y+d) c ys
+hom = coerce hom'
+
+hom' :: Integer -> Integer -> Integer -> Integer -> [Integer] -> [Integer]
+hom' 1 0 0 1 xs = xs
+hom' _ _ 0 0 _  = []
+hom' a b c d xs
+  | c /= 0, d /= 0
+  , q <- quot a c, q == quot b d
+  = q : hom' c d (a - c*q) (b - d*q) xs
+  | otherwise = case xs of
+    []   -> hom' a a c c []
+    y:ys -> hom' (a*y+b) a (c*y+d) c ys
+
+-- | Gosper-style bihomographic transformations
+--
+-- @
+-- z = axy + bx + cy + d
+--     -----------------
+--     exy + fx + gy + h
+-- @
+--
+-- TODO: detect cycles
+bihom :: Integer -> Integer -> Integer -> Integer
+      -> Integer -> Integer -> Integer -> Integer -> CF -> CF -> CF
+bihom = coerce bihom 
+
+bihom' :: Integer -> Integer -> Integer -> Integer
+   -> Integer -> Integer -> Integer -> Integer -> [Integer] -> [Integer] -> [Integer]
+bihom' a b _ _ e f _ _ xs [] = hom' a b e f xs
+bihom' a _ c _ e _ g _ [] ys = hom' a c e g ys
+bihom' 0 1 0 0 0 0 0 1 xs _  = xs
+bihom' 0 0 1 0 0 0 0 1 _  ys = ys
+bihom' a b c d e f g h xs@(x:xs') ys@(y:ys')
+   | e /= 0, f /= 0, g /= 0, h /= 0 
+   , q <- quot a e, q == quot b f
+   , q == quot c g, q == quot d h
+   = q : bihom' e f g h (a-q*e) (b-q*f) (c-q*g) (d-q*h) xs ys
+   | e /= 0 || f /= 0
+   , (e == 0 && g == 0) || abs (g*e*b - g*a*f) > abs (f*e*c - g*a*f)
+   = bihom' (a*x+b) a (c*x+d) c (e*x+f) e (g*x+h) g xs' ys
+   | otherwise
+   = bihom' (a*y+c) (b*y+d) a b (e*y+g) (f*y+h) e f xs ys'
 
 -- | Brent's teleporting turtle
 brent :: Eq a => [a] -> Bool
 brent [] = False
 brent (x:xs) = go 1 2 x xs where
+  go :: Eq a => Int -> Int -> a -> [a] -> Bool
   go k n t [] = False
   go k n t (r:rs) = r == t || if k == n
     then go 1 (n * 2) r rs
     else go (k + 1) n t rs
 
 instance Num CF where
-  (+) = bihom 0 1 1 0 0 0 0 1    -- (x+y)/1
-  (-) = bihom 0 1 (-1) 0 0 0 0 1 -- (x-y)/1
-  (*) = bihom 1 0 0 0 0 0 0 1    -- (x*y)/1
-  negate (CF xs)      = CF (map negate xs)
+  (+) = bihom
+    0 1 1 0
+    0 0 0 1    -- (x+y)/1
+  (-) = bihom
+    0 1 (-1) 0
+    0 0 0    1 -- (x-y)/1
+  (*) = bihom
+    1 0 0 0
+    0 0 0 1    -- (x*y)/1
+  negate (CF xs)      = CF (map negate xs) -- hom (-1) 0 0 1 -- -x/1 
   abs (CF as)         = CF (map abs as)
   signum (CF [])      = CF [1]
   signum (CF [0])     = CF [0]
@@ -171,13 +180,15 @@ instance Num CF where
 instance Fractional CF where
   recip (CF (0:as)) = CF as
   recip (CF as) = CF (0:as)
-  (/) = bihom 0 1 0 0 0 0 1 0 -- x / y
-  fromRational (k :% n) = CF (rat k n)
-
-rat :: Integer -> Integer -> [Integer]
-rat k 0 = []
-rat k n = case k `quotRem` n of
-  (q, r) -> q : if r == 0 then [] else rat n r
+  (/) = bihom
+     0 1 0 0
+     0 0 1 0 -- x/y
+  fromRational (k0 :% n0) = CF (go k0 n0) where
+    go k 0 = []
+    go k n = case k `quotRem` n of
+      (q, r) -> q : if r == 0
+        then []
+        else go n r
 
 instance Enum CF where
   succ = hom 1 1 0 1    -- (x+1)/1
@@ -185,3 +196,26 @@ instance Enum CF where
   fromEnum (CF (n:_)) = fromIntegral n
   fromEnum (CF [])    = maxBound
   toEnum = fromIntegral
+
+{-
+-- | Compute a homographic equation with continued fraction coefficients.
+cfhom :: CF -> CF -> CF -> CF -> [CF] -> [CF]
+cfhom 1 0 0 1 xs = xs
+cfhom _ _ 0 0 _  = []
+cfhom a b c d xs 
+  | c /= 0, d /= 0
+  , q <- quot a c
+  , q == quot b d
+
+
+-- | the moment two subsequent rationals are have a given integer residue we stop?
+properFraction' :: CF -> (Integer, CF)
+properFraction (CF []) = (0, CF [])
+properFraction' (CF (a:as)) = case compare a 0 of
+  LT -> (a - 1, hom (-1) 1 0 1 (0:as))
+  EQ -> case as of
+    [] -> (0,0)
+    (b:bs) | b < 0 -> -- negative 
+           | b > 0 -> positive
+  GT -> (a, CF (0:as))
+-}
